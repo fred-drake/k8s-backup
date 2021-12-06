d20cb1fdcf125c339c98e0f3096240f15d288f0c
Fred Drake <fred.drake@gmail.com>
2021-12-05 20:25:21 -0500

    Fixed bracket bug

--- init.sh
+++ init.sh
@@ -5,27 +5,27 @@ if [ -z "$NAMESPACE" ]; then
 fi
 
 if [ "$BACKUP_TYPE" == "postgresql" ]; then
-    if [ -z "$PG_HOST "]; then
+    if [ -z "$PG_HOST" ]; then
         echo "PG_HOST environment variable is not set!"
         exit 1;
     fi
 
-    if [ -z "$PG_DATABASE "]; then
+    if [ -z "$PG_DATABASE" ]; then
         echo "PG_DATABASE environment variable is not set!"
         exit 1;
     fi
 
-    if [ -z "$PG_USER "]; then
+    if [ -z "$PG_USER" ]; then
         echo "PG_USER environment variable is not set!"
         exit 1;
     fi
 
-    if [ -z "$PG_PASSWORD "]; then
+    if [ -z "$PG_PASSWORD" ]; then
         echo "PG_PASSWORD environment variable is not set!"
         exit 1;
     fi
 
-    if [ -z "$PG_BACKUP_FILE "]; then
+    if [ -z "$PG_BACKUP_FILE" ]; then
         echo "PG_BACKUP_FILE environment variable is not set!"
         exit 1;
     fi
@@ -36,14 +36,14 @@ if [ -z "$BACKUP_TYPE" ]; then
     BACKUP_TYPE="volume"
 fi
 
-if [ -z "$BACKUP_DIRECTORY"]; then
+if [ -z "$BACKUP_DIRECTORY" ]; then
     BACKUP_DIRECTORY=/backup
 fi
 
-if [ -z "$SLEEP_BETWEEN_BACKUPS"]; then
+if [ -z "$SLEEP_BETWEEN_BACKUPS" ]; then
     SLEEP_BETWEEN_BACKUPS=86400
 fi
 
-if [ -z "$PG_PORT"]; then
+if [ -z "$PG_PORT" ]; then
     PG_PORT=5432
 fi
