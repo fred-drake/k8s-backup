06f33d078219f9284ef6878ac3d40c4902147586
Fred Drake <fred.drake@gmail.com>
2021-12-07 23:50:12 -0500

    adding db to pg_dump

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ while true; do
             echo "${PG_HOST}:${PG_PORT}:${PG_DATABASE}:${PG_USER}:${PG_PASSWORD}" > ~/.pgpass
             chmod 600 ~/.pgpass
             mkdir -p /backup/${NAMESPACE}/postgresql
-            pg_dump > /backup/${NAMESPACE}/postgresql/${PG_BACKUP_FILE}
+            pg_dump ${PG_DATABASE} > /backup/${NAMESPACE}/postgresql/${PG_BACKUP_FILE}
         elif [ "$TYPE" == "prometheus" ]; then
             echo "$(date +"%Y-%m-%d %H:%m") | ${NAMESPACE} | Taking Prometheus snapshot"
             rm -rf /backup/${NAMESPACE}/snapshots
