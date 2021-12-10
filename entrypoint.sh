d32904b78fe38e2f391be6e4c9c39f4384a2c2bb
Fred Drake <fred.drake@gmail.com>
2021-12-09 19:36:58 -0500

    Updated restic tags

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ while true; do
     done
 
     echo "$(date +"%Y-%m-%d %H:%m") | ${NAMESPACE} | Beginning restic backup"
-    restic backup ${BACKUP_DIRECTORY} --tag automated,${NAMESPACE}
+    restic backup ${BACKUP_DIRECTORY} --tag automated --tag ${NAMESPACE}
     echo "$(date +"%Y-%m-%d %H:%m") | ${NAMESPACE} | Backup completed. Sleeping ${SLEEP_BETWEEN_BACKUPS} seconds."
     sleep ${SLEEP_BETWEEN_BACKUPS}
 done
