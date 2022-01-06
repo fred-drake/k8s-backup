faec68e83074391edf7f606477e5d107d8889048
Fred Drake <fred.drake@gmail.com>
2022-01-05 21:02:30 -0500

    Set PG_PORT on dump and not call restic if error

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,23 +5,28 @@ source /app/init.sh
 
 echo "$(date +"%Y-%m-%d %H:%M") | ${NAMESPACE} | Starting application"
 while true; do 
+    OKAY=1
     for TYPE in "${BACKUP_TYPES_ARRAY[@]}"; do
         if [ "$TYPE" == "postgresql" ]; then
             echo "$(date +"%Y-%m-%d %H:%M") | ${NAMESPACE} | Beginning PostgreSQL database dump"
-            echo "${PG_HOST}:${PG_PORT}:${PG_DATABASE}:${PG_USER}:${PG_PASSWORD}" > ~/.pgpass
-            chmod 600 ~/.pgpass
-            mkdir -p /backup/${NAMESPACE}/postgresql
-            pg_dump --username ${PG_USER} --host ${PG_HOST} ${PG_DATABASE} > /backup/${NAMESPACE}/postgresql/${PG_BACKUP_FILE}
+            echo "${PG_HOST}:${PG_PORT}:${PG_DATABASE}:${PG_USER}:${PG_PASSWORD}" > ~/.pgpass || OKAY=0
+            chmod 600 ~/.pgpass || OKAY=0
+            mkdir -p /backup/${NAMESPACE}/postgresql || OKAY=0
+            pg_dump --username ${PG_USER} --host ${PG_HOST} --port ${PG_PORT} ${PG_DATABASE} > /backup/${NAMESPACE}/postgresql/${PG_BACKUP_FILE} || OKAY=0
         elif [ "$TYPE" == "prometheus" ]; then
             echo "$(date +"%Y-%m-%d %H:%M") | ${NAMESPACE} | Taking Prometheus snapshot"
-            rm -rf /backup/${NAMESPACE}/snapshots
+            rm -rf /backup/${NAMESPACE}/snapshots || OKAY=0
             echo "$(date +"%Y-%m-%d %H:%M") | ${NAMESPACE} | Return status: $(curl -XPOST --silent http://prometheus-server/api/v1/admin/tsdb/snapshot | jq .status)"
-            mv /backup/${NAMESPACE}/snapshots/* /backup/${NAMESPACE}/snapshots/snapshot
+            mv /backup/${NAMESPACE}/snapshots/* /backup/${NAMESPACE}/snapshots/snapshot || OKAY=0
         fi
     done
 
-    echo "$(date +"%Y-%m-%d %H:%M") | ${NAMESPACE} | Beginning restic backup"
-    restic backup ${BACKUP_DIRECTORY} --tag automated --tag ${NAMESPACE}
+    if [ "$OKAY" == 1 ]; then
+        echo "Errors were found previously.  Not running restic backup."
+    else
+        echo "$(date +"%Y-%m-%d %H:%M") | ${NAMESPACE} | Beginning restic backup"
+        restic backup ${BACKUP_DIRECTORY} --tag automated --tag ${NAMESPACE}
+    fi
     echo "$(date +"%Y-%m-%d %H:%M") | ${NAMESPACE} | Backup completed. Sleeping ${SLEEP_BETWEEN_BACKUPS} seconds."
     sleep ${SLEEP_BETWEEN_BACKUPS}
 done
